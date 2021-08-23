45ad595e45315578b3ddd5ec561d1c0624f82f34
Atthis <blanchard.ced@gmail.com>
2021-08-23 22:24:28 +0200

    add repeat functions

--- app.js
+++ app.js
@@ -2,35 +2,36 @@ import { songList } from './musiclist.mjs';
 
 const trckImg = document.querySelector('.current-media__img');
 const trckName = document.querySelector('.current-media__title');
-const playBtn = document.querySelector('.play-pause-btn');
-const prevBtn = document.querySelector('.fa-step-backward');
-const nextBtn = document.querySelector('.fa-step-forward');
 const progressBar = document.querySelector('.progress-bar__progress');
 const currTime = document.querySelector('.progress-bar__current-time');
 const totalTime = document.querySelector('.progress-bar__total-time');
+const playBtn = document.querySelector('.play-pause-btn');
+const prevBtn = document.querySelector('.fa-step-backward');
+const nextBtn = document.querySelector('.fa-step-forward');
+const repeatBtn = document.querySelector('.fa-redo');
 
 // Creation de l'element audio
 const currentTrack = document.createElement('audio');
 
 let trackIndex = 0;
-let playing = false; // pour affichage picto lecture/pause
 let progressTimer; // timer pour MAJ barre progression
+let playing = false; // pour affichage picto lecture/pause
+let repeatOn = 0;
 
 // Chargement de la piste au chargement de la page + MAJ des infos
-function loadTrack(){
+function loadTrack() {
   // RAZ de l'intervale pour l'update de la barre de temps
   clearInterval(progressTimer);
-  
+
   currentTrack.src = songList[trackIndex].trackSource;
   trckImg.src = songList[trackIndex].trackImg;
   trckName.innerText = `${songList[trackIndex].trackName} 
   ${songList[trackIndex].trackArtist}`;
 
-  
   // Update de la barre de temps
   progressTimer = setInterval(timeUpdate, 1000);
 
-  // Lancement de la piste suivante à la fin
+  // Lancement de la piste suivante à la fin ou de la meme si repeat actif
   currentTrack.addEventListener('ended', nextTrack);
 }
 
@@ -56,61 +57,96 @@ function pauseTrack() {
   playing = false;
 }
 
-function timeUpdate (){
-  let timePosition = 0;// RAZ de la valeur du temps de la piste en cours
-  if(!isNaN(currentTrack.duration)){
-     timePosition = (currentTrack.currentTime*100)/currentTrack.duration;
+function timeUpdate() {
+  let timePosition = 0; // RAZ de la valeur du temps de la piste en cours
+  if (!isNaN(currentTrack.duration)) {
+    timePosition = (currentTrack.currentTime * 100) / currentTrack.duration;
     progressBar.value = timePosition;
-  
-  // MAJ des infos duree
-  currTime.innerText = toMinutes(currentTrack.currentTime);
-  totalTime.innerText = toMinutes(currentTrack.duration);
-  
-     // MAJ de la barre de progression
-     progressBar.style.backgroundSize = `${timePosition}% 100%`;
+
+    // MAJ des infos duree
+    currTime.innerText = toMinutes(currentTrack.currentTime);
+    totalTime.innerText = toMinutes(currentTrack.duration);
+
+    // MAJ de la barre de progression
+    progressBar.style.backgroundSize = `${timePosition}% 100%`;
   }
 }
 
 // convertir le temps de seconde à minutes:secondes
-function toMinutes(time){
- let minutes = Math.floor(time/60);
-  let seconds = Math.floor(time - (minutes*60));
-  
-  if (minutes<10){minutes = `0${minutes}`;}
-  if (seconds<10){seconds = `0${seconds}`;}
-  
+function toMinutes(time) {
+  let minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time - minutes * 60);
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
   return `${minutes}:${seconds}`;
 }
 
 // fonction pour selection du temps de la piste par l'utilisateur
-function changeTime(){
+function changeTime() {
   // recuperation du clic de l'utilisateur
-  let userTime = currentTrack.duration * progressBar.value /100;
-  
+  let userTime = (currentTrack.duration * progressBar.value) / 100;
+
   // Definition de la durée en cours
   currentTrack.currentTime = userTime;
 }
 
-
 // Piste suivante et precedente
-function nextTrack(){
-  // Controle si arrive en fin de liste de lecture
-  if(trackIndex < songList.length -1){
-    trackIndex += 1;
-  } else { trackIndex = 0;}
+function nextTrack() {
+  // variable activant la répétition de playlist ou non
+  let noPlaylistRepeat = true;
+
+  switch (repeatOn) {
+    case 0: // Si aucun repeat activé
+      // Controle si arrive en fin de liste de lecture
+      if (trackIndex < songList.length - 1) {
+        trackIndex++;
+      } else {
+        trackIndex = 0;
+        noPlaylistRepeat = true;
+      }
+      break;
+    case 1:
+      if (trackIndex === songList.length - 1) {
+        trackIndex = 0;
+        noPlaylistRepeat = false;
+      } else {
+        trackIndex++;
+      }
+      break;
+    case 2:
+      noPlaylistRepeat = true;
+      break;
+    default:
+      console.log('Valeur non-reconnue');
+  }
 
   // chargement de la nouvelle piste
   loadTrack(trackIndex);
 
-  // lecture de la piste
-  playTrack();
+  if (noPlaylistRepeat && trackIndex === 0 && repeatOn !== 2) {
+    // Derniere piste atteinte et pas de repetition de playlist
+    currentTrack.pause();
+    currentTrack.currentTime = 0;
+  } else {
+    // lecture de la piste
+    playTrack();
+    console.log(repeatOn);
+  }
 }
 
 function prevTrack() {
   // Controle si arrive en fin de liste de lecture
-  if(trackIndex > 0){
-    trackIndex -= 1;
-  } else { trackIndex = songList.length -1;}
+  if (trackIndex > 0) {
+    trackIndex--;
+  } else {
+    trackIndex = songList.length - 1;
+  }
 
   // chargement de la nouvelle piste
   loadTrack(trackIndex);
@@ -119,13 +155,36 @@ function prevTrack() {
   playTrack();
 }
 
+function repeatTrack() {
+  if (repeatOn >= 2) {
+    repeatOn = 0;
+    repeatBtn.style.color = '#fff';
+    repeatBtn.innerText = '';
+    console.log(repeatOn);
+  } else {
+    repeatOn++;
+
+    if (repeatOn === 1) {
+      repeatBtn.style.color = '#95c1c4';
+      repeatBtn.innerText = ' all';
+      console.log(repeatOn);
+    }
+    if (repeatOn === 2) {
+      repeatBtn.style.color = '#95c1c4';
+      repeatBtn.innerText = ' 1';
+      console.log(repeatOn);
+    }
+  }
+}
 
 window.addEventListener('load', loadTrack);
 playBtn.addEventListener('click', playPauseTrack);
 progressBar.addEventListener('change', changeTime);
 
 prevBtn.addEventListener('click', prevTrack);
-nextBtn.addEventListener('click', prevTrack);
+nextBtn.addEventListener('click', nextTrack);
+
+repeatBtn.addEventListener('click', repeatTrack);
 
 // Random function for shuffle :
 // return (trckImg.src = songList[Math.floor(Math.random() * songList.length)].trackImg);
--- musiclist.mjs
+++ musiclist.mjs
@@ -1,23 +1,5 @@
 const trackList = [
   {
-    trackName: 'Waiting',
-    trackArtist: 'Crowander',
-    trackAlbum: 'Acoustic Miniband',
-    trackDuration: 1.28,
-    trackSource: 'track/Crowander-Waiting.mp3',
-    trackImg:
-      'https://images.unsplash.com/photo-1433048980017-63f162f662b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-  },
-  {
-    trackName: 'Parody',
-    trackArtist: 'Crowander',
-    trackAlbum: 'Acoustic Miniband',
-    trackDuration: 2.02,
-    trackSource: 'track/Crowander-Parody.mp3',
-    trackImg:
-      'https://images.unsplash.com/photo-1433883669848-fa8a7ce070b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
-  },
-  {
     trackName: 'The Sattler Monument (instrumental)',
     trackArtist: 'Robin Allender',
     trackAlbum: '-',
@@ -35,6 +17,24 @@ const trackList = [
     trackImg:
       'https://images.unsplash.com/photo-1480487453552-4d3c7d1d65e1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
   },
+  {
+    trackName: 'Waiting',
+    trackArtist: 'Crowander',
+    trackAlbum: 'Acoustic Miniband',
+    trackDuration: 1.28,
+    trackSource: 'track/Crowander-Waiting.mp3',
+    trackImg:
+      'https://images.unsplash.com/photo-1433048980017-63f162f662b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+  },
+  {
+    trackName: 'Parody',
+    trackArtist: 'Crowander',
+    trackAlbum: 'Acoustic Miniband',
+    trackDuration: 2.02,
+    trackSource: 'track/Crowander-Parody.mp3',
+    trackImg:
+      'https://images.unsplash.com/photo-1433883669848-fa8a7ce070b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
+  }
 ];
 
 export const songList = trackList;
